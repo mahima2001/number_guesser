4ed3a24bdf3d1a6df6594afbeec0e2d97184f04e
mahima2001 <43854083+mahima2001@users.noreply.github.com>
2021-04-14 12:04:49 +0530

    ''

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
+let min=1,max=10,winningNum=2,guessesLeft=3;
 
+const game =document.getElementById('game'),
+    minNum=document.querySelector('.min-num'),
+    maxNum=document.querySelector('.max-num'),
+    guessBtn=document.querySelector('#guess-btn'),
